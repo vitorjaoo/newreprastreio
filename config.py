e5e64a5c5b9578097cd4fa55e3497b4cd688bc6a
vitorjaoo <129765259+vitorjaoo@users.noreply.github.com>
2026-03-17 15:33:46 -0300

    Update SECRET_KEY default value in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY          = os.getenv("SECRET_KEY", "troque-esta-chave-em-producao")
+    SECRET_KEY          = os.getenv("SECRET_KEY", "limarepresentacoes..")
     TURSO_DATABASE_URL  = os.getenv("libsql://rasrep-vitorrastrep.aws-us-east-2.turso.io")
     TURSO_AUTH_TOKEN    = os.getenv("eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzM3NzIxNDIsImlkIjoiMDE5Y2Y2YjgtMWQwMS03YTRhLTlmMTAtZDU3ZDRiYzUwZWM4IiwicmlkIjoiNWUwZmFiOGEtZGMwZS00MmI2LTlmMWMtZDFiNmU4ZjMwMjY3In0.YsrURbRJdjYAKkNnR9dEb_jlAL4sUPC52pbFDh6LnUYEeo87dUTENFy55kNvB5B5TTIic-Oe6P1CDsMxgMJ-BA")
     ADMIN_SENHA         = os.getenv("ADMIN_SENHA", "admin123")
